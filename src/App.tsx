58e2d161eeed663c22bc3ddab6755412a7bc1785
Justin Magaram <justin@magaram.com>
2021-02-03 11:10:46 -0800

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,48 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import "./App.css";
 import { CategoryManager } from "./components/CategoryManager";
-import { DataStore } from "aws-amplify";
-import { Category } from "./models";
 
 function App() {
   return (
-    <div>
-      <h1>Groceries</h1>
-      <CategoryManager></CategoryManager>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/categories">Categories</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/categories">
+            <CategoryManager />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
 }
 
 export default App;
